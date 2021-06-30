c13f555e3b850b741633c95879b1667362581504
penghao <118724111.@qq.com>
2021-06-30 18:11:54 +0800

    添加eureka-server7002 与eureka-server7001组成双机集群

---
+++ cloud-eureka-server7002/src/main/java/com/halo/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.halo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author halo
+ * @date 2021/6/30 17:11
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
