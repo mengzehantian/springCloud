d8fe12c39118cd439f1753e7dcf4612b69943425
penghao <118724111.@qq.com>
2021-06-30 16:26:31 +0800

    添加Eureka单机注册服务

---
+++ cloud-eureka-server7001/src/main/java/com/halo/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.halo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author halo
+ * @date 2021/6/30 16:05
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
