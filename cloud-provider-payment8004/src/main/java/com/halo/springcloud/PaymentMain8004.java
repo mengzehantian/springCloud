504b74a833e629f174bcc4b8a7aa767f574934b6
penghao <118724111.@qq.com>
2021-07-06 09:45:38 +0800

    提交paymanet8004，遗漏的主函数

---
+++ cloud-provider-payment8004/src/main/java/com/halo/springcloud/PaymentMain8004.java
@@ -0,0 +1,19 @@
+package com.halo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author halo
+ * @date 2021/7/6 8:59
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
