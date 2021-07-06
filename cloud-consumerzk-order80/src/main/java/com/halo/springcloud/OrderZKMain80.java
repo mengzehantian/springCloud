4f665e5a204f991cecbaebbc85919b35be3874b9
penghao <118724111.@qq.com>
2021-07-06 09:59:08 +0800

    创建 cloud-consumerzk-order80订单服务模块，并注册进入zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/halo/springcloud/OrderZKMain80.java
@@ -0,0 +1,18 @@
+package com.halo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author halo
+ * @date 2021/7/5 19:41
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class,args);
+    }
+}
---
+++ cloud-consumerzk-order80/src/main/java/com/halo/springcloud/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.halo.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author halo
+ * @date 2021/7/5 19:45
+ * @description
+ */
+@RestController
+public class OrderController {
+
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/zk")
+    public String paymentInfo()
+    {
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/zk",String.class);
+        return result;
+    }
+}
