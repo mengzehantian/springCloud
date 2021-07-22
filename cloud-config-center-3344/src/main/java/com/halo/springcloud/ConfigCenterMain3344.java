74adeda943f6be5491029a418bf01817d1672be8
penghao <118724111.@qq.com>
2021-07-22 18:06:05 +0800

    config配置总控中心搭建

---
+++ cloud-config-center-3344/src/main/java/com/halo/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.halo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author halo
+ * @date 2021/7/22 16:43
+ * @description
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
